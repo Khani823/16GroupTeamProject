2823f330a703dbfafa24e4938f9794ec8a8ef6b3
chamgilom <149364808+chamgilom@users.noreply.github.com>
2024-01-03 13:35:14 +0900

    [ADD]Inventory

---
+++ Assets/02. Scripts/hyeongjin/UserData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "UserData_", menuName = "Data/UserData", order = 2)]
+public class UserData : ScriptableObject
+{
+    public string UserName;
+
+}
