2fbb1b5cded5cc8e0b4f2650add6b7d411b1ac17
Limdeokhan <limdh001@gmail.com>
2024-01-04 17:13:36 +0900

    Add MouseMove

--- Assets/02. Scripts/ClickMove.cs
+++ Assets/02. Scripts/ClickMove.cs
@@ -10,17 +10,23 @@ public class ClickMove : MonoBehaviour
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) 
+        if (Input.GetMouseButtonDown(0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-            if(Physics.Raycast(ray, out RaycastHit hit))
+            // 충돌한 지점의 정보를 저장
+            RaycastHit2D hit2d = Physics2D.Raycast(mousePosition, Vector2.zero);
+
+            if (hit2d.collider != null && hit2d.collider.CompareTag("Ground"))
             {
-                agent.SetDestination(hit.point);        
+                // 네비게이션 메시 에이전트의 목적지 설정
+                agent.SetDestination(hit2d.point);
             }
         }
     }
