a63710b9c55226fe11de9f4254194e9e429a4db9
Limdeokhan <limdh001@gmail.com>
2024-01-08 16:59:42 +0900

    Add Grid Movement

---
+++ Assets/02. Scripts/Deokhan/FreeMovement.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeMovement : MonoBehaviour
+{
+    private float speed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            MovePlayer(Vector3.up);
+        if (Input.GetKey(KeyCode.A))
+            MovePlayer(Vector3.left);
+        if (Input.GetKey(KeyCode.S))
+            MovePlayer(Vector3.down);
+        if (Input.GetKey(KeyCode.D))
+            MovePlayer(Vector3.right);
+
+    }
+
+    private void MovePlayer(Vector3 direction)
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
